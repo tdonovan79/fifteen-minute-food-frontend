8ea6a2200969800284cb0d14e48158c9c236b4c2
tdonovan79 <tdonovan79@gmail.com>
2020-03-02 13:31:25 -0500

    stripe working

---
+++ src/Payment.js
@@ -0,0 +1,55 @@
+// => imports
+import React, { useState } from 'react'
+import StripeCheckout from 'react-stripe-checkout';
+
+// => URLs
+const BASE_URL = 'http://localhost:3000'
+const CHARGES_URL = BASE_URL + '/charge_adapter'
+
+// => app component
+export default function Payment() {
+
+    const [price, setPrice] = useState(100)
+
+    const handlePrice = (event) => {
+        setPrice(event.target.value)
+    }
+
+    const onToken = (token) => {
+
+        const charge = {
+            token: token.id
+        };
+
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ charge: charge, price: price * 100 })
+        };
+
+        fetch(CHARGES_URL, config)
+        .then(res => res.json())
+        .then(console.log)
+    }
+
+    return (
+        <div>
+            <form>
+                <label>Price: </label>
+                <input
+                    type="number"
+                    value={ price }
+                    onChange={ handlePrice }
+                />
+            </form>
+
+            <StripeCheckout
+                token={onToken}
+                // stripeKey={process.env.STRIPE_KEY}
+                stripeKey={'pk_test_IPcPeFrETWjmr9mFIYFwZlqn00nfwg4xZi'}
+            />
+        </div>
+    )
+}
