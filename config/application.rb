6bdd51f615bf3f16747872b8d846ee078bc35cd9
tdonovan79 <tdonovan79@gmail.com>
2020-03-03 11:33:09 -0500

    cors in application

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,16 @@ module FifteenMinBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options, :head]
+          )
+      end
+    end
   end
 end
