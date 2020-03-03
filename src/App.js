e6f049b2c7ddfefbaf7d17d6923edbb43e77af23
tdonovan79 <tdonovan79@gmail.com>
2020-03-03 09:27:14 -0500

    cart and checkout pages

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import Form from './Components/Form'
 import { withRouter } from 'react-router-dom'
+import CartPage from './Orders/CartPage.js'
+import CheckOut from './Orders/CheckOut.js'
+
 class App extends React.Component {
     state = {
         user: {
             username: "",
             id: 0
         },
-        token: ""
+        token: "",
+        itemsInCart: [{ id: 0, name: "burger", price: 12 }, { id: 1, name: "pizza", price: 45 }]
+    }
+    //delete item by id passed up from CartItem
+    onDeleteItem = (itemId) => {
+        let newItemList = this.state.itemsInCart.filter(item => {
+            return item.id != itemId
+        })
+        this.setState({
+            itemsInCart: newItemList
+        })
     }
     componentDidMount() {
         if (localStorage.token) {
-            fetch(`http://localhost:4000/persist`, {
+            fetch(`http://localhost:3000/persist`, {
                 headers: {
                     'Authorization': `Bearer ${localStorage.token}`
                 }
@@ -32,7 +45,7 @@ class App extends React.Component {
     }
     handleLoginSubmit = (logUser) => {
         console.log(logUser)
-        fetch(`http://localhost:4000/login`, {
+        fetch(`http://localhost:3000/login`, {
             method: "POST",
             headers: {
                 "content-type": "application/json"
@@ -44,7 +57,7 @@ class App extends React.Component {
     }
     handleRegisterSubmit = (newUser) => {
         console.log(newUser)
-        fetch(`http://localhost:4000/users`, {
+        fetch(`http://localhost:3000/users`, {
             method: "POST",
             headers: {
                 "content-type": "application/json"
@@ -61,6 +74,14 @@ class App extends React.Component {
             return <Form formName="Register Form" handleSubmit={this.handleRegisterSubmit} />
         }
     }
+    //render cart
+    renderCartPage = () => {
+        return <CartPage onDeleteItem={this.onDeleteItem} itemsInCart={this.state.itemsInCart}/>
+    }
+    //render checkout
+    renderCheckout = () => {
+        return <CheckOut itemsInCart={this.state.itemsInCart}/>
+    }
     render() {
         console.log(this.props)
         return (
@@ -70,8 +91,11 @@ class App extends React.Component {
                         <Route path="/login" render={this.renderForm} />
                         <Route path="/register" render={this.renderForm} />
                         <Route path="/profile" render={this.renderProfile} />
+                        <Route path="/cart" render={this.renderCartPage}/>
+                        <Route path="/checkout" render={this.renderCheckout}/>
                     </Switch>
                 </header>
+                
             </div>
         );
     }
