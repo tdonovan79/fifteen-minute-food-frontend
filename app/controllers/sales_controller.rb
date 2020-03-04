15a7381f7e67fa5ccc6d6bc0e1e67d4c7ab88cac
tdonovan79 <tdonovan79@gmail.com>
2020-03-04 17:26:48 -0500

    re-migration

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,22 +1,23 @@
 class SalesController < ApplicationController
     # before_action 
     # :authorized, only: [:persist]
-   
-  def index
-    sale = Sale.all 
 
-    render json: sale
-  end
+    def index
+        sale = Sale.all 
+
+        render json: sale
+    end
 
     def create
         # byebug
-       if User.all.find_by(username: sale_params[:user])
+        if User.all.find_by(username: sale_params[:user])
             @user = User.all.find_by(username: sale_params[:user])
             if  Restaurant.all.find_by(address: sale_params[:restaurant][:address])
                 @restaurant = Restaurant.all.find_by(address: sale_params[:restaurant][:address])
                 @sale = Sale.create(user: @user, restaurant: @restaurant)
 
                 params[:items].each do |item| 
+                    byebug
                     FoodItem.create(name: item[:name], price: item[:price], sale: @sale)
                 end
                 
@@ -28,10 +29,10 @@ class SalesController < ApplicationController
                     FoodItem.create(name: item[:name], price: item[:price], sale: @sale)
                 end
             end
+            render json: @sale
         else
             render json: { error: "NOPE" }
         end
-
     end
         
 
