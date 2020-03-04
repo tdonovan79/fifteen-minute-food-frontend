f0e378a943b783c65bb01524cb6d9245f560643f
ShaquilleClarke <github email address>
2020-03-04 16:52:28 -0500

    nice

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,20 +1,15 @@
 class Sale < ApplicationRecord
     belongs_to :restaurant
     belongs_to :user
-<<<<<<< HEAD
     has_many :food_items
 
-    def create_sale
-        Sale.create(sales_params)
-    end
+    # def create_sale
+    #     Sale.create(sales_params)
+    # end
 
-    private
+    # private
 
-    def sale_params
-        params.permit(:restaurant_id, :user_id)
-    end
-=======
-    
-    has_many :food_items
->>>>>>> fif_backend_5
+    # def sale_params
+    #     params.permit(:restaurant_id, :user_id)
+    # end
 end
