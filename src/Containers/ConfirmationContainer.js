07cb595cf0d7c7bb090f1bf180f868d510afa471
tdonovan79 <tdonovan79@gmail.com>
2020-03-05 11:49:28 -0500

    user bug fixed

--- src/Containers/ConfirmationContainer.js
+++ src/Containers/ConfirmationContainer.js
@@ -3,37 +3,31 @@ import React, { Component } from 'react'
 export default class ConfirmationContainer extends Component {
 
     state = {
-        user: "",
         restaurant: "",
         food: []
     }
-        
-        
+
+
 
     componentDidMount() {
         fetch("http://localhost:3000/confirm")
-        .then(r => r.json())
-        .then(order => {
-            this.setState({
-                user: order.user.username,
-                restaurant: order.restaurant.name,
-                food: order.food_items
+            .then(r => r.json())
+            .then(order => {
+                this.setState({
+                    restaurant: order.restaurant.name,
+                    food: order.food_items
+                })
             })
-        })
     }
 
     render() {
         let foodOrder = this.state.food
-        // foodOrder.map(food => {
-        //     return <li>{food.name}</li>
-        // })
-        //clear cart and selected 
         return (
             <div>
-                <h1>Thank you {this.state.user}!</h1> 
+                <h1>Thank you {localStorage.username}!</h1>
                 <h2>for your order of</h2>
                 <ul>{foodOrder.map(food => {
-                return <li>{food.name}</li>
+                    return <li>{food.name}</li>
                 })}</ul>
                 <h2>from</h2>
                 <h1>{this.state.restaurant}</h1>
